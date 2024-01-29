fafe2b21997a3e0359c7099288d8c365425e5647
idreesahmef1257 <idreesahmef1257@gmail.com>
2024-01-29 23:59:29 +0500

    Video Resolved

--- src/Components/Home/VideoPage.js
+++ src/Components/Home/VideoPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Player } from 'video-react';
 import 'video-react/dist/video-react.css';
-
+import video from '../../assets/video1.mp4'
 
 const VideoPage = () => {
-  const videoSource = "/src/assets/video1.mp4";
+  const videoSource = "/src/assets/";
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-full lg:w-1/2 h-3/4 lg:h-1/2">
-        <Player playsInline src={videoSource}>
+        {/* <Player playsInline src={videoSource}>
           <source src={videoSource} type="video/mp4" />
-        </Player>
+        </Player> */}
+        <video muted autoPlay src={video} width="750" height="500" controls>
+        </video>
       </div>
     </div>
   );
